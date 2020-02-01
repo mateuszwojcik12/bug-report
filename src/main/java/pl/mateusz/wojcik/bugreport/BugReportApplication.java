d9895b354745c1e39d3ddfd5f8063ad0b8189752
Mateusz Wójcik <mateusz-wojcik@outlook.com>
2020-02-01 09:29:39 +0100

    project init

---
+++ src/main/java/pl/mateusz/wojcik/bugreport/BugReportApplication.java
@@ -0,0 +1,13 @@
+package pl.mateusz.wojcik.bugreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugReportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BugReportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/mateusz/wojcik/bugreport/BugReportApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mateusz.wojcik.bugreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugReportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
